1154823938eaf03d6726a71ee7a8b579df1962d6
Tim <tim_ren@ehsy.com>
2016-04-22 14:14:23 +0800

    Initial Commit

---
+++ Simple/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Simple
+//
+//  Created by ehsy on 16/4/22.
+//  Copyright © 2016年 ehsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Simple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Simple
+//
+//  Created by ehsy on 16/4/22.
+//  Copyright © 2016年 ehsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
