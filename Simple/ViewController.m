6be9262820a5dbbfc30600025c3d04e8b419ac6e
Tim <tim_ren@ehsy.com>
2016-04-26 10:40:39 +0800

    提交masonry添加

--- Simple/ViewController.m
+++ Simple/ViewController.m
@@ -16,8 +16,103 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a ni
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.view.translatesAutoresizingMaskIntoConstraints = YES;
+    [self addSimpleDemo];
 }
+
+//masonry简单运用
+- (void)addSimpleDemo
+{
+    WS(ws);
+    UIView *sv = [UIView new];
+    sv.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:sv];
+    [sv mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(ws.view);
+        make.size.mas_equalTo(CGSizeMake(300, 300));
+    }];
+    
+    
+//    UIView *sv1 = [UIView new];
+//    sv1.backgroundColor = [UIColor redColor];
+//    [sv addSubview:sv1];
+//    [sv1 mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.edges.equalTo(sv).with.insets(UIEdgeInsetsMake(10, 10, 10, 10));
+//    }];
+//
+//    int padding1 = 10;
+//    UIView *sv2 = [UIView new];
+//    sv2.backgroundColor = [UIColor orangeColor];
+//    [sv addSubview:sv2];
+//    UIView *sv3 = [UIView new];
+//    sv3.backgroundColor = [UIColor orangeColor];
+//    [sv addSubview:sv3];
+//    [sv2 mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.centerY.mas_equalTo(sv.mas_centerY);
+//        make.left.equalTo(sv.mas_left).with.offset(padding1);
+//        make.right.equalTo(sv3.mas_left).with.offset(-padding1);
+//        make.height.mas_equalTo(@150);
+//        make.width.equalTo(sv3);
+//    }];
+//    [sv3 mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.centerY.mas_equalTo(sv.mas_centerY);
+//        make.left.equalTo(sv2.mas_right).with.offset(padding1);
+//        make.right.equalTo(sv.mas_right).with.offset(-padding1);
+//        make.height.mas_equalTo(@150);
+//        make.width.equalTo(sv2);
+//    }];
+
+    
+    
+    UIScrollView *scrollView = [UIScrollView new];
+    scrollView.backgroundColor = [UIColor whiteColor];
+    [sv addSubview:scrollView];
+    [scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(sv).with.insets(UIEdgeInsetsMake(5, 5, 5, 5));
+    }];
+    
+    UIView *container = [UIView new];
+    [scrollView addSubview:container];
+    [container mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(scrollView);
+        make.width.equalTo(scrollView);
+    }];
+    
+    int count = 10;
+    UIView *lastView = nil;
+    for (int i = 0; i<=count; ++i) {
+        UIView *subV = [UIView new];
+        [container addSubview:subV];
+        subV.backgroundColor = [UIColor colorWithHue:( arc4random() % 256 / 256.0 )
+                                          saturation:( arc4random() % 128 / 256.0 ) + 0.5
+                                          brightness:( arc4random() % 128 / 256.0 ) + 0.5
+                                               alpha:1];
+        
+        [subV mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.and.right.equalTo(container);
+            make.height.mas_equalTo(@(20*i));
+            if (lastView) {
+                make.top.mas_equalTo(lastView.mas_bottom);
+            }else{
+                make.top.mas_equalTo(container.mas_top);
+            }
+        }];
+        lastView = subV;
+    }
+    [container mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(lastView.mas_bottom);
+    }];
+}
+
+
+//masonry中级运用
+-(void)addMediumDemo
+{
+
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
