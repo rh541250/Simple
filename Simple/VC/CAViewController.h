647102ce01755ad88c66cfbfa9aaa5b569167051
Tim <tim_ren@ehsy.com>
2016-05-13 15:49:07 +0800

    提交动画修改

---
+++ Simple/VC/CAViewController.h
@@ -0,0 +1,13 @@
+//
+//  CAViewController.h
+//  Simple
+//
+//  Created by ehsy on 16/5/11.
+//  Copyright © 2016年 ehsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAViewController : UIViewController
+
+@end
